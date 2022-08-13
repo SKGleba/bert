458e14ec91333d4c0c6cd673b83c8bd3acd567fe
Sebastian Kubala <30833773+SKGleba@users.noreply.github.com>
2022-08-13 20:33:00 +0200

    added a command handler and packet beautifier

--- jigclient.py
+++ jigclient.py
@@ -42,6 +42,8 @@ key3table = {
     0xF: "EBE3460D84A41754AC441368CF0200D8"
 }
 
+CMD900_PWD = "93CE8EBEDF7F69A96F35DDE3BECB97D5"
+
 
 def read_all(port, size):
     data = b''
@@ -49,6 +51,9 @@ def read_all(port, size):
         data += port.read(size - len(data))
     assert len(data) == size
     return data
+    
+def print_packet(pinfo, cmdp):
+    print(pinfo + ": 0x" + cmdp[2:4] + cmdp[0:2] + " 0x" + cmdp[4:6] + " 0x" + cmdp[8:10] + cmdp[6:8] + " [ " + cmdp[10:-4] + " ] 0x" + cmdp[-2:] + cmdp[-4:-2])
 
 class JIGClient(object):
     def __init__(self, uart_port):
@@ -75,80 +80,109 @@ class JIGClient(object):
         x= bytes.fromhex(self.read_line().decode("ascii"))
         return x
 
-    def send_cmd(self, cmd):
-
+    def send_cmd(self, cmd, do_format):
         #cmd = cmd.encode('ascii')
         bcmd = cmd + self.checksum(cmd)
         #bcmd += ':{0:02X}\r\n'.format(self.checksum(cmd)).encode('ascii')
         uartcmd = bcmd.hex().upper() + "\r\n"
-        print("SEND: " + uartcmd[0:-2])
+        if do_format == True:
+            print_packet("SEND", uartcmd[:-2])
+        elif do_format == False:
+            print("SEND: " + uartcmd[0:-2])
         #print('send: {0}'.format(cmd[:-1].decode('ascii')))
         self._port.write(uartcmd.encode("ascii"))
-
+        
 
 client = JIGClient('COM6')
 
-cmd = sys.argv[1]
-if cmd == "unlock1":
-    c = bytearray.fromhex("0301000000")
-    client.send_cmd(c)
+def send_simple_cmd(cmd_ascii):
+    c = bytearray.fromhex(cmd_ascii)
+    client.send_cmd(c, True)
     response = client.get_resp()
-    print("RESP: " + response.hex().upper())
- 
-elif cmd == "lock1":
-    c = bytearray.fromhex("0401000000")
-    client.send_cmd(c)
-    response = client.get_resp()
-    print("RESP: " + response.hex().upper())
-   
-elif cmd == "handshake":
-    #                      0 1 2 3 4 5 6 7 8 9 A B C D E F 101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D
-    #                      10010050003001000E00000000BABEF00DBEEFBABE000000000000000000000000000000000000000000000000
-    c = bytearray.fromhex("100100500030000000000000000000000000000000000000000000000000000000000000000000000000000000")
-    c[8] = 0xE
-    client.send_cmd(c)
-    response =client.get_resp()
-    print("RESP: " + response.hex().upper())
-    if response[2] != 0:
-        print("ERROR: Bad Response (" + response[2].hex() + ")")
-        exit(0)
-    c[6] = 1
-    c[0xD:0x15] = random.randbytes(8)
-    # Send step 1 challenge
-    client.send_cmd(c)
-    response =client.get_resp()
-    print("RESP: " + response.hex().upper())
-    if response[2] != 0 or response[7] !=0:
-        print("ERROR: Bad Response (" + response[2].hex() + "-" + response[7].hex() + ")")
-        exit(0)
-
-    payload = response[0xD:0x2D]
+    print_packet("RESP", response.hex().upper())
     
-    shared_secret = bytes.fromhex(secret1data[c[8]])
-    key1 = bytes.fromhex(key1table[c[8]])
-    key2 = bytes.fromhex(key2table[c[8]])
-    iv = bytes(0x10)
-    plain = AES.new(key1, AES.MODE_CBC, iv).decrypt(payload)
-    if plain[0x10:0x20] == shared_secret:
-        handplain = plain[8:0x10] + plain[0:8] + iv
-        handcrypt = AES.new(key2, AES.MODE_CBC, iv).encrypt(handplain)
-        c[6] = 3
-        c[0xD:0x2D] = handcrypt
-        client.send_cmd(c)
-        response =client.get_resp()
-        print("RESP: " + response.hex().upper())
-        if response[7] == 0 and response[6] == 0xFF:
-            print("Successful Auth")
-        else:
-            print("ERROR: Failed Auth (" + response[2].hex() + "-" + response[6].hex() + "-"+ response[7].hex() + ")")
-            
-                
-    else:
-        print("ERROR: Invalid Handshake secret")
-
+def handle_cmd(user_cmd):
+    match user_cmd:
+        case "getinfo":
+            send_simple_cmd("0101000000")
+        case "lockall": # TODO: make sure it only resets auth...
+            send_simple_cmd("6101000000")
+        case "unlock1":
+            send_simple_cmd("0301000000")
+        case "lock1":
+            send_simple_cmd("0401000000")
+        case "unlock4":
+            send_simple_cmd("0009002000" + CMD900_PWD)
+        case "lock4":
+            send_simple_cmd("0109000000")
         
-else:
-    client.send_cmd(bytearray.fromhex(cmd))            
+        case "unlock8":
+            print("Starting 3Auth")
+            #                      0 1 2 3 4 5 6 7 8 9 A B C D E F 101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D
+            #                      10010050003001000E00000000BABEF00DBEEFBABE000000000000000000000000000000000000000000000000
+            c = bytearray.fromhex("100100500030000000000000000000000000000000000000000000000000000000000000000000000000000000")
+            c[8] = 0xE
+            client.send_cmd(c, True)
+            response =client.get_resp()
+            print_packet("RESP", response.hex().upper())
+            if response[2] != 0:
+                print("ERROR: Bad Response (" + response[2].hex() + ")")
+                exit(0)
+            c[6] = 1
+            c[0xD:0x15] = random.randbytes(8)
+            # Send step 1 challenge
+            client.send_cmd(c, True)
+            response =client.get_resp()
+            print_packet("RESP", response.hex().upper())
+            if response[2] != 0 or response[7] !=0:
+                print("ERROR: Bad Response (" + response[2].hex() + "-" + response[7].hex() + ")")
+                exit(0)
+
+            payload = response[0xD:0x2D]
+    
+            shared_secret = bytes.fromhex(secret1data[c[8]])
+            key1 = bytes.fromhex(key1table[c[8]])
+            key2 = bytes.fromhex(key2table[c[8]])
+            iv = bytes(0x10)
+            plain = AES.new(key1, AES.MODE_CBC, iv).decrypt(payload)
+            if plain[0x10:0x20] == shared_secret:
+                handplain = plain[8:0x10] + plain[0:8] + iv
+                handcrypt = AES.new(key2, AES.MODE_CBC, iv).encrypt(handplain)
+                c[6] = 3
+                c[0xD:0x2D] = handcrypt
+                client.send_cmd(c, True)
+                response =client.get_resp()
+                print_packet("RESP", response.hex().upper())
+                if response[7] == 0 and response[6] == 0xFF:
+                    print("Successful 3Auth")
+                else:
+                    print("ERROR: Failed 3Auth (" + response[2].hex() + "-" + response[6].hex() + "-"+ response[7].hex() + ")")
+            
+            else:
+                print("ERROR: Invalid Handshake secret")
+
+        case _:
+            send_simple_cmd(user_cmd)
+
+cmd = sys.argv[1]
+
+if cmd == "raw":
+    client.send_cmd(bytearray.fromhex(sys.argv[2]), False)           
     response =client.get_resp()
     print("RESP: " + response.hex().upper())
+elif cmd == "unlockall":
+    handle_cmd("unlock1")
+    handle_cmd("unlock8")
+    handle_cmd("unlock4")
+elif cmd[:2] == "0x":
+    cv_cmd = sys.argv[1][4:6] + sys.argv[1][2:4] + sys.argv[2][2:4] + sys.argv[3][4:6] + sys.argv[3][2:4]
+    if len(sys.argv) >= 5:
+        cv_cmd += sys.argv[4]
+    handle_cmd(cv_cmd)
+else:
+    handle_cmd(cmd)
+    
 
+#client.send_cmd(cmd)
+#response = client.read_line()
+#print(response.decode("ascii"), end ='')
