85040107068ba967792b4b3f3ae6761cc5a62b34
Sebastian Kubala <30833773+SKGleba@users.noreply.github.com>
2022-10-02 03:12:01 +0200

    added interactive mode prototoype

--- jigclient.py
+++ jigclient.py
@@ -9,6 +9,7 @@ import serial, time, re, math
 from Crypto.Cipher import AES
 import random
 
+DEFAULT_PORT = 'COM14'
 
 secret1data = {
     0x0: "80996FBBC8B4EBA30595F4D379A23BD0",
@@ -44,7 +45,6 @@ key3table = {
 
 CMD900_PWD = "93CE8EBEDF7F69A96F35DDE3BECB97D5"
 
-
 def read_all(port, size):
     data = b''
     while len(data) < size:
@@ -56,11 +56,18 @@ def print_packet(pinfo, cmdp):
     print(pinfo + ": 0x" + cmdp[2:4] + cmdp[0:2] + " 0x" + cmdp[4:6] + " 0x" + cmdp[8:10] + cmdp[6:8] + " [ " + cmdp[10:-4] + " ] 0x" + cmdp[-2:] + cmdp[-4:-2])
 
 class JIGClient(object):
-    def __init__(self, uart_port):
-        self._port = serial.Serial(uart_port, baudrate=38400, timeout=0)
+    def __init__(self):
+        self.is_open = False
         
-    def deinit(self):
-        self._port.close()
+    def open(self, uart_port, baud):
+        if self.is_open == False:
+            self._port = serial.Serial(uart_port, baudrate=baud, timeout=0)
+            self.is_open = True
+        
+    def close(self):
+        if self.is_open == True:
+            self._port.close()
+            self.is_open = False
 
     def read_all(self, size):
         return read_all(self._port, size)
@@ -90,10 +97,12 @@ class JIGClient(object):
             print_packet("SEND", uartcmd[:-2])
         elif loglevel == 1:
             print("SEND: " + uartcmd[0:-2])
+        self._port.reset_input_buffer()
+        self._port.reset_output_buffer()
         self._port.write(uartcmd.encode("ascii"))
         
 
-client = JIGClient('COM7')
+client = JIGClient()
 
 def cmd_x110(keyv):
     print("Starting 3Auth")
@@ -145,69 +154,133 @@ def send_simple_cmd(cmd_ascii):
     response = client.get_resp()
     print_packet("RESP", response.hex().upper())
     
-def handle_cmd(user_cmd):
+
+def test():
+    for x in range(0, 0xB60, 0x20):
+        cv_cmd = "3101000600" + "{:02X}".format(x & 0xFF) + "{:02X}".format((x & 0xFF00) // 0x100) + "20"
+        client.send_cmd(bytearray.fromhex(cv_cmd), 0)
+        response =client.get_resp()
+        print(response.hex()[10:-4].upper())
+
+def handle_cmd(user_cmd, argv):
+    if client.is_open == False:
+        client.open(DEFAULT_PORT, 38400)
     match user_cmd:
         case "raw":
-            client.send_cmd(bytearray.fromhex(sys.argv[2]), 1)           
+            client.send_cmd(bytearray.fromhex(argv[2]), 1)           
             response = client.get_resp()
             print("RESP: " + response.hex().upper())
-        case "getinfo":
+        case "test":
+            test()
+        case "nop":
+            send_simple_cmd("0001000000")
+        case "info":
             send_simple_cmd("0101000000")
-        case "unlock1":
+        case "unlock-1":
             send_simple_cmd("0301000000")
-        case "lock1":
+        case "lock-1":
             send_simple_cmd("0401000000")
+        case "power-get":
+            send_simple_cmd("0601000000")
+        case "power-off":
+            send_simple_cmd("050100020000")
+        case "power-on":
+            send_simple_cmd("050100020002")
+        case "power-sus":
+            send_simple_cmd("050100020003")
         case "reset":
             client.send_cmd(bytearray.fromhex("6201000000"), 2)
-        case "unlock4":
+        case "unlock-4":
             send_simple_cmd("0009002000" + CMD900_PWD)
-        case "lock4":
+        case "lock-4":
             send_simple_cmd("0109000000")
-        case "handshakeE":
+        case "handshake-E":
             cmd_x110(0xE)
-        case "handshake0":
+        case "handshake-0":
             cmd_x110(0x0)
-        case "handshake1":
+        case "handshake-1":
             cmd_x110(0x1)
-        case "unlockqa":
-            handle_cmd("unlock1")
-            handle_cmd("handshakeE")
-        case "unlocknvs":
-            handle_cmd("unlock1")
-            handle_cmd("handshake1")
-        case "unlocksdboot":
-            handle_cmd("unlock1")
-            handle_cmd("handshake0")
-        case "unlockall":
-            handle_cmd("unlock1")
-            handle_cmd("handshakeE")
-            handle_cmd("handshake1")
-            handle_cmd("handshake0")
-            handle_cmd("unlock4")
-        case "readnvs":
-            cv_cmd = "3101000600" + sys.argv[2][4:6] + sys.argv[2][2:4] + sys.argv[3][2:4]
+        case "unlock-qa":
+            handle_cmd("unlock-1", argv)
+            handle_cmd("handshake-E", argv)
+        case "unlock-nvs":
+            handle_cmd("unlock-1", argv)
+            handle_cmd("handshake-1", argv)
+        case "unlock-sdboot":
+            handle_cmd("unlock-1", argv)
+            handle_cmd("handshake-0", argv)
+        case "unlock-all":
+            handle_cmd("unlock-1", argv)
+            handle_cmd("handshake-E", argv)
+            handle_cmd("handshake-1", argv)
+            handle_cmd("handshake-0", argv)
+            handle_cmd("unlock-4", argv)
+        case "nvs-read":
+            cv_cmd = "3101000600" + argv[2][4:6] + argv[2][2:4] + argv[3][2:4]
             client.send_cmd(bytearray.fromhex(cv_cmd), 0)
             response =client.get_resp()
             print(response.hex()[10:-4].upper())
-        case "writenvs":
-            cv_cmd = "320100" + "{:02X}".format((int(sys.argv[3][2:4], 16) * 2) + 6) + "00" + sys.argv[2][4:6] + sys.argv[2][2:4] + sys.argv[3][2:4] + sys.argv[4]
+        case "nvs-read-range":
+            cv_bsz = int(argv[4][2:], 16)
+            cv_argv = [""] * 4
+            for x in range(int(argv[2][2:], 16), int(argv[3][2:], 16), cv_bsz):
+                cv_argv[0] = "nvs-read-range"
+                cv_argv[1] = "nvs-read"
+                cv_argv[2] = "0x" + "{:04X}".format(x)
+                cv_argv[3] = "0x" + "{:02X}".format(cv_bsz)
+                handle_cmd(cv_argv[1], cv_argv)
+        case "nvs-write":
+            cv_cmd = "320100" + "{:02X}".format((int(argv[3][2:4], 16) * 2) + 6) + "00" + argv[2][4:6] + argv[2][2:4] + argv[3][2:4] + argv[4]
             client.send_cmd(bytearray.fromhex(cv_cmd), 0)
             response =client.get_resp()
             print(response.hex()[4:6].upper())
         case _:
-            send_simple_cmd(user_cmd)
+            if user_cmd[:2] == "0x":
+                cv_cmd = argv[1][4:6] + argv[1][2:4] + argv[2][2:4] + argv[3][4:6] + argv[3][2:4]
+                if len(argv) >= 5:
+                    cv_cmd += argv[4]
+                return handle_cmd(cv_cmd, argv)
+            else:
+                send_simple_cmd(user_cmd)
 
-cmd = sys.argv[1]
+def interactive():
+    print("\nWelcome to interactive mode")
+    print("use !help to display usage info")
+    while 1:
+        print("")
+        uinput = input('> ')
+        uinput_argv = uinput.split()
+        uinput_argv.insert(0, "interactive")
+        cmd = uinput_argv[1]
+        match cmd:
+            case "!exit":
+                break
+            case "!open":
+                client.open(uinput_argv[2], int(uinput_argv[3]))
+            case "!close":
+                client.close()
+            case "!wait":
+                if client.is_open == True:
+                    line = client.get_resp()
+                    if len(uinput_argv) > 2:
+                        print_packet("ERNIE", line.hex().upper())
+                    else:
+                        print("ERNIE: " + line.hex().upper())
+                else:
+                    print("port not open!")
+            case _:
+                handle_cmd(cmd, uinput_argv)
+                
 
-if cmd[:2] == "0x":
-    cv_cmd = sys.argv[1][4:6] + sys.argv[1][2:4] + sys.argv[2][2:4] + sys.argv[3][4:6] + sys.argv[3][2:4]
-    if len(sys.argv) >= 5:
-        cv_cmd += sys.argv[4]
-    handle_cmd(cv_cmd)
+uinput_argv = sys.argv           
+cmd = uinput_argv[1]
+
+if cmd[:1] == "!":
+    interactive()
 else:
-    handle_cmd(cmd)
+    handle_cmd(cmd, uinput_argv)
     
-client.deinit()
+client.close()
     
 
 #client.send_cmd(cmd)
